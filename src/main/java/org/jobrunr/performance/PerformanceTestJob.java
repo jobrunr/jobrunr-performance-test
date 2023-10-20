58a962e29d374d086080a877e3de5b725b4bc03d
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2023-10-20 23:41:26 +0200

    Updates

--- src/main/java/org/jobrunr/performance/PerformanceTestJob.java
+++ src/main/java/org/jobrunr/performance/PerformanceTestJob.java
@@ -2,13 +2,6 @@ package org.jobrunr.performance;
 
 import com.codahale.metrics.Meter;
 import org.jobrunr.jobs.annotations.Job;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.time.Duration;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class PerformanceTestJob {
 
