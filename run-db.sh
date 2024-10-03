aa1ce97b112574de49a4426e553b0dcfe2b1f31e
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2024-10-03 17:21:04 +0200

    Update run-db.sh to automatically clean docker volume

--- run-db.sh
+++ run-db.sh
@@ -1,7 +1,6 @@
-docker stop postgres-pghero
-docker rm postgres-pghero
-docker stop postgres-performance
-docker rm postgres-performance
+docker rm -f postgres-pghero
+docker rm -f postgres-performance
+docker volume prune -f
 # docker run --name postgres-performance -p 5432:5432 -v "/home/dev/jobrunr/jobrunr-pro-performance-test/my-postgres.conf":/etc/postgresql/postgresql.conf -e POSTGRES_PASSWORD=andrien09 --shm-size=1g -d postgres -c "config_file=/etc/postgresql/postgresql.conf"
 docker run --name postgres-performance -p 5432:5432 -e POSTGRES_PASSWORD=oTsMa6h61BOFYTpIVvLs --shm-size=1g -d postgres -c "shared_preload_libraries=pg_stat_statements"
 POSTGRES_IP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' postgres-performance)
