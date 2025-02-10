e1ef715093c28b1ee88b3fad0046ff5aeb13cfbe
rdehuyss <ronald.dehuysser@gmail.com>
2025-02-10 10:16:21 +0100

    WIP testing

---
+++ run/scenario01/pro/all-but-slow-v1.0.0-SNAPSHOT.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="$(dirname "$(dirname "$(dirname "$SCRIPT_DIR")")")"
+
+time mvn -f "$ROOT_DIR/pom.xml" compile exec:java@performance-test -P PRO -Dexec.args="jvm-per-datastore=true datastore=allButSlow scenario=Scenario01ProcessJobs amount=500_000 dashboard_port=8010"
