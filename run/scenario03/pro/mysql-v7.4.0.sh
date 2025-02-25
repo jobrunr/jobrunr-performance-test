e39e8755521fcb36db169185096ba861a4cfd435
rdehuyss <ronald.dehuysser@gmail.com>
2025-02-25 22:42:23 +0100

    WIP testing

--- run/scenario03/pro/mysql-v7.4.0.sh
+++ run/scenario03/pro/mysql-v7.4.0.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(dirname "$(dirname "$(dirname "$SCRIPT_DIR")")")"
 
-time mvn -f "$ROOT_DIR/pom.xml" compile exec:java@performance-test -P PRO -Djobrunr.version=7.4.0 -Dexec.args="datastore=MySQLDataStore scenario=Scenario03ConcurrentJobRateLimiter amount=100_000 dashboard_port=8010"
+time mvn -f "$ROOT_DIR/pom.xml" compile exec:java@performance-test -P PRO -Djobrunr.version=7.4.0 -Dexec.args="datastore=MySQLDataStore scenario=Scenario03ConcurrentJobRateLimiter amount=440_000 dashboard_port=8010"
