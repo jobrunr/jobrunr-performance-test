2f1a36659466cdccd429d9996850a3df5bdcb258
rdehuyss <ronald.dehuysser@gmail.com>
2025-03-24 18:03:11 +0100

    Try to improve Oracle

---
+++ src/main/resources/oracle/container-entrypoint-initdb.d/02_alter_redo_log.sql
@@ -0,0 +1,4 @@
+ALTER DATABASE ADD LOGFILE GROUP 3 ('/opt/oracle/oradata/FREE/redo03.log') SIZE 1024M;
+ALTER DATABASE ADD LOGFILE GROUP 4 ('/opt/oracle/oradata/FREE/redo04.log') SIZE 1024M;
+
+ALTER SYSTEM switch logfile;
