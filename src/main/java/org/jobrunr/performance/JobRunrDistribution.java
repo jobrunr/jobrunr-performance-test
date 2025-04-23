d78f4c947e77d1b378eb8d1cdb417c74254e3cbf
rdehuyss <ronald.dehuysser@gmail.com>
2025-04-23 22:38:23 +0200

    Improve testing Releem

--- src/main/java/org/jobrunr/performance/JobRunrDistribution.java
+++ src/main/java/org/jobrunr/performance/JobRunrDistribution.java
@@ -17,7 +17,7 @@ public enum JobRunrDistribution {
         public void saveLicense(StorageProvider storageProvider) {
             String license = getVersion().endsWith("-SNAPSHOT")
                     ? "eyJhbGciOiJFQyIsImNydiI6ICJQLTI1NiIsInR5cCI6ICJKV1QifQ.eyJzdWJzY3JpcHRpb25JZCI6IjMxOTI5ZDYzLWRhMDItNDYwMi04ZjliLWYyMzk1YTY3YzUwOSIsImNvbXBhbnkiOiJSb3NvY28gQlYiLCJ0cmlhbCI6ZmFsc2UsInZhbGlkVW50aWwiOiIyMDI1LTA2LTMwIn0.zMBypBCY9n3qX4d7KXyGsHGLmfeauGVmOq5sz7Lszeo0sKXsoTyu4djKCcnWIbO9BOpye65i4QUJRwjT73Yu3w=="
-                    : "eyJhbGciOiJFQyIsImNydiI6ICJQLTI1NiIsInR5cCI6ICJKV1QifQ.eyJzdWJzY3JpcHRpb25JZCI6IjFmNDM3NzczLWI3Y2YtNGIyZC04NjQxLWEyNGI0ZWQzN2U0OSIsImNvbXBhbnkiOiJKb2JSdW5yIFBybyBQZXJmb3JtYW5jZSBUZXN0IExpY2Vuc2UiLCJ0cmlhbCI6ZmFsc2UsInZhbGlkVW50aWwiOiIyMDI1LTA3LTAyIn0.jWlM6snU7SndfKVM0xqsgz3OVM_KqxBfm4AChCmm0ER0fDvfn1hM-_Y5U7DVxgGp6VHVkdgUZDCywoM5sSXHpg==";
+                    : "eyJhbGciOiJFQyIsImNydiI6ICJQLTI1NiIsInR5cCI6ICJKV1QifQ.eyJzdWJzY3JpcHRpb25JZCI6IjFmNDM3NzczLWI3Y2YtNGIyZC04NjQxLWEyNGI0ZWQzN2U0OSIsImNvbXBhbnkiOiJKb2JSdW5yIFBybyBQZXJmb3JtYW5jZSBUZXN0IExpY2Vuc2UiLCJ0cmlhbCI6ZmFsc2UsInZhbGlkVW50aWwiOiIyMDI1LTA3LTAyIn0.eYnjjfEnm-R_GdQ4f7EJ4AmBRCoZldkJFGI2Pgiq4mn7B0MdRBt5BbSJHHBvdazLc0b7QYeeX8B_RkQLpmW-HA==";
 
             // To be compatible with v6
             try {
