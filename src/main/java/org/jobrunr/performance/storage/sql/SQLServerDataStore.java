182e6f9f7d6bb5662750ceb0808d45ab9c490f9d
rdehuyss <ronald.dehuysser@gmail.com>
2025-05-01 10:22:45 +0200

    Improve testing scenario

--- src/main/java/org/jobrunr/performance/storage/sql/SQLServerDataStore.java
+++ src/main/java/org/jobrunr/performance/storage/sql/SQLServerDataStore.java
@@ -58,4 +58,9 @@ public class SQLServerDataStore extends AbstractSqlDataStore {
             return sb.toString();
         }
     }
+
+    @Override
+    public boolean isQueryUsingIndex(String analysis, String indexName) {
+        return analysis.contains("Index=\"[" + indexName + "]\"");
+    }
 }
