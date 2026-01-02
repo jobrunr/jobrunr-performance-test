91f82f1b1ae4070161410f12b81e6045e358335a
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2026-01-02 11:07:25 +0100

    Improve code

--- run-db.sh
+++ run-db.sh
@@ -3,7 +3,6 @@ docker rm -f postgres-performance
 docker volume prune -f
 # docker run --name postgres-performance -p 5432:5432 -v "/home/dev/jobrunr/jobrunr-pro-performance-test/my-postgres.conf":/etc/postgresql/postgresql.conf -e POSTGRES_PASSWORD=andrien09 --shm-size=1g -d postgres -c "config_file=/etc/postgresql/postgresql.conf"
 docker run --name postgres-performance -p 5432:5432 -e POSTGRES_PASSWORD=oTsMa6h61BOFYTpIVvLs --shm-size=1g -d postgres -c "shared_preload_libraries=pg_stat_statements"
-POSTGRES_IP=$(docker inspect --format='{{.NetworkSettings.IPAddress}}' postgres-performance)
-docker run --name postgres-pghero -e DATABASE_URL=postgres://postgres:oTsMa6h61BOFYTpIVvLs@"$POSTGRES_IP":5432/postgres -p 8090:8080 -d ankane/pghero
+docker run --name postgres-pghero -e DATABASE_URL=postgres://postgres:oTsMa6h61BOFYTpIVvLs@host.docker.internal:5432/postgres -p 8090:8080 -d ankane/pghero
 sleep 1
 
